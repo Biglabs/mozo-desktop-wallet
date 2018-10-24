57b5d3ea8a6cc6f75b3ded525c65aad1e00bb35b
Duc Minh Tran <duc.tm@big-labs.com>
2018-10-24 14:44:12 +0700

    Trim the value of the input address Trim the value of the input address

--- src/ui/screens/transaction/CreateTransactionScreen.web.js
+++ src/ui/screens/transaction/CreateTransactionScreen.web.js
@@ -142,7 +142,8 @@ export default class CreateTransactionScreen extends React.Component {
       };
 
     doCreateTransaction() {
-        let to_address = this.state.to_address;
+        let to_address = this.state.to_address.trim();
+        this.state.to_address = to_address;
         let mozo_value = this.state.mozo_value;
         if (to_address == this._from_address) {
             return;
