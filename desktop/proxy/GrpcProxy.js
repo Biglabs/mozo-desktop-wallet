b25d072de78d1b2d661e8ee56330ec553af9d335
Duc Minh Tran <duc.tm@big-labs.com>
2018-11-21 16:13:32 +0700

    Add Access-Control-Expose-Headers for Chrome browser Add Access-Control-Expose-Headers for Chrome browser

--- desktop/proxy/GrpcProxy.js
+++ desktop/proxy/GrpcProxy.js
@@ -47,7 +47,9 @@ let httpServer = null;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers",
-             "Origin, X-Requested-With, Content-Type, Accept");
+             "Origin, X-Requested-With, Content-Type, Accept, x-total-count");
+  res.header("Access-Control-Expose-Headers",
+             "Origin, X-Requested-With, Content-Type, Accept, x-total-count");
   next();
 });
 
